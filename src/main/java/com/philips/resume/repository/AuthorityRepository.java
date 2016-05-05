1f4f122cb30af1a208c6963f51f532ed184e8c7c
Lin <QiZhong.LIN@philips.com>
2016-05-05 21:33:21 +0800

    init project resume via jHipster3.1.0

---
+++ src/main/java/com/philips/resume/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.philips.resume.repository;
+
+import com.philips.resume.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
